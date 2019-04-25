fdefed413e287f7eb373299a7a3b04842d03986d
Matt D <dwinell.matt@gmail.com>
2019-04-25 00:01:17 -0400

    can now generate opponent image, generating image displays name underneath image and hides generate button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
     };
     firebase.initializeApp(config);
     database = firebase.database();
-
+var generator = "null";
     //setting the initial player and opponent objects. we can add more attributes to pass in as needed.
     var player = {
         name: "bob",
@@ -26,7 +26,7 @@ $(document).ready(function () {
     var opponent = {
         name: "",
         armorClass: 0,
-        hitpoints: 0,
+        hitPoints: 0,
         damageDice: "1d4",
         attackBonus: 0,
         stillImageUrl: "",
@@ -35,8 +35,11 @@ $(document).ready(function () {
 // on click event for generating user character
     $("#generate-user-character").on("click", function (event) {
         event.preventDefault();
+        generator = "player"
         var userImage = $("#user-image-holder").attr("src");
         if (userImage == "") {
+            $("#generate-user-character").css("visibility", "hidden");
+            $("#user-image-holder").css("visibility", "visible");
             var randomIndex = Math.ceil(Math.random() * 325);
             var dndURL = "https://cors.io/?http://dnd5eapi.co/api/monsters/" + randomIndex + "/";
             $.ajax({
@@ -69,20 +72,68 @@ $(document).ready(function () {
                 player.name = player.name.split(' ').join('+');
                 console.log(player.name);
                 giphyURL = "https://api.giphy.com/v1/gifs/random?apikey=ZyUXN606XVdEZHZ5sk3RWjOKSzOOFOyk&tag=" + player.name;
-                setTimeout (googleRetrieve, 1000);
+                setTimeout (userGoogleRetrieve, 1000);
+                $("#player-name").text("Your body has become that of a " + player.name.split("+").join(' ') + ".").css("visibility", "visible");
 
             })
         }
     })
 //on click event for generating opponent's character
 
+$("#generate-opponent-character").on("click", function (event) {
+    event.preventDefault();
+    generator = "opponent";
+    var opponentImage = $("#opponent-image-holder").attr("src");
+    if (opponentImage == "") {
+        $("#generate-opponent-character").css("visibility", "hidden");
+        $("#opponent-image-holder").css("visibility", "visible");
+        var randomIndex = Math.ceil(Math.random() * 325);
+        var dndURL = "https://cors.io/?http://dnd5eapi.co/api/monsters/" + randomIndex + "/";
+        $.ajax({
+            url: dndURL,
+            method: "GET"
+        }).then(function (response) {
+            var result = JSON.parse(response);
+            console.log(result, result.name);
+            opponent.name = result.name;
+            opponent.armorClass = result.armor_class;
+            opponent.hitPoints = result.hit_points;
+            if (result.actions[0].damage_dice) {
+                opponent.damageDice = result.actions[0].damage_dice;
+                opponent.attackBonus = result.actions[0].attack_bonus;
+                opponent.damageBonus = result.actions[0].damage_bonus;
+            } else if (result.actions[1].damage_dice) {
+                opponent.damageDice = result.actions[1].damage_dice;
+                opponent.attackBonus = result.actions[1].attack_bonus;
+                opponent.damageBonus = result.actions[1].damage_bonus;
+            } else if (result.actions[2].damage_dice) {
+                opponent.damageDice = result.actions[2].damage_dice;
+                opponent.attackBonus = result.actions[2].attack_bonus;
+                opponent.damageBonus = result.actions[2].damage_bonus;
+            } else {
+                opponent.damageDice = "1d4"
+                opponent.attackBonus = 0;
+            }
+            console.log(opponent);
+            console.log(opponent.name);
+            opponent.name = opponent.name.split(' ').join('+');
+            console.log(opponent.name);
+            giphyURL = "https://api.giphy.com/v1/gifs/random?apikey=ZyUXN606XVdEZHZ5sk3RWjOKSzOOFOyk&tag=" + opponent.name;
+            setTimeout (opponentGoogleRetrieve, 1000);
+            $("#opponent-name").text("The " + opponent.name.split('+').join(' ') + " has entered the arena.").css("visibility", "visible");
+
+
+        })
+    }
+})
 
 
 
     //setting google retrieve as a separate function to be called one second after first api call- to prevent .then problems.
-function googleRetrieve(){
+function userGoogleRetrieve(){
+    
     var googleUrl = "https://www.googleapis.com/customsearch/v1?key=AIzaSyAaUUgoWlaZBGGzG4HNuQUN3Jcrw4NH7zU&cx=008788128101746337676:gz0k2znll90&q=dnd+" + player.name + "=1&searchType=image&num=2";
-    console.log(googleUrl);
+    console.log(googleUrl, player.name);
     $.ajax({
         url: googleUrl,
         method: "GET"
@@ -92,6 +143,23 @@ function googleRetrieve(){
         console.log(googleresponse.items[0].link);
         player.stillImageUrl = googleresponse.items[0].link;
         $("#user-image-holder").attr("src", player.stillImageUrl);
+       // console.log(giphyresponse.data.images.fixed_height_still.url);
+    });
+    
+}
+function opponentGoogleRetrieve(){
+    
+    var googleUrl = "https://www.googleapis.com/customsearch/v1?key=AIzaSyAaUUgoWlaZBGGzG4HNuQUN3Jcrw4NH7zU&cx=008788128101746337676:gz0k2znll90&q=dnd+" + opponent.name + "=1&searchType=image&num=2";
+    console.log(googleUrl, opponent.name);
+    $.ajax({
+        url: googleUrl,
+        method: "GET"
+
+    }).then(function(googleresponse){
+        console.log(googleresponse);
+        console.log(googleresponse.items[0].link);
+        opponent.stillImageUrl = googleresponse.items[0].link;
+        $("#opponent-image-holder").attr("src", opponent.stillImageUrl);
        // console.log(giphyresponse.data.images.fixed_height_still.url);
     });
     

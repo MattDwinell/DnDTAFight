9508b4f5d8425625176f72a9898ae4888713d6ff
MattDwinell <dwinell.matt@gmail.com>
2021-02-16 19:39:02 -0500

    removing cors.io domain prefix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -104,7 +104,7 @@ $(document).ready(function () {
             $("#opponent-name").css("visibility", "visible");
             $("#opponent-hp").css("visibility", "visible");
             var randomIndex = Math.ceil(Math.random() * 325);
-            var dndURL = "https://cors.io/?http://dnd5eapi.co/api/monsters/" + randomIndex + "/";
+            var dndURL = "?http://dnd5eapi.co/api/monsters/" + randomIndex + "/";
             $.ajax({
                 url: dndURL,
                 method: "GET"
